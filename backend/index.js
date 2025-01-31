5d1bfbd404bf94474aa08786b4e114c5cf425776
nikunj kumar <nikunjkumar2911@gmail.com>
2025-01-31 18:32:13 +0530

    final

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ const freekaViewroute = require('./Routers/freekaViewroute.js');
 const ProfilesRouter = require('./Routers/ProfilesRouter.js');
 
 const corsOptions = {
-    origin: 'http://localhost:3001', //frontend url
+    origin: 'https://www.muslimmalikrishte.com', //frontend url
     methods: 'GET, POST, PUT, DELETE,PATCH', 
     credentials: true, 
 };
@@ -28,7 +28,7 @@ app.use(cors(corsOptions));
 app.use(cookieParser());
 app.use(fileUpload({useTempFiles:true}));
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3001'); // your frontend URL
+    res.header('Access-Control-Allow-Origin', 'https://www.muslimmalikrishte.com'); // your frontend URL
     res.header('Access-Control-Allow-Credentials', 'true');
     next();
 });
